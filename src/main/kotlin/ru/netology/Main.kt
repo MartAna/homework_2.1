509de8f079f317b9783e082fd52d752debaa1da7
Анастасия Мартынкевич <semenova.a.sergeevna@gmail.com>
2022-04-10 21:40:55 +0300

    Create task

---
+++ src/main/kotlin/ru/netology/Main.kt
@@ -0,0 +1,28 @@
+package ru.netology
+
+fun main() {
+    println(sizeCommission(100_000, 0, "Mastercard"))
+    println(sizeCommission(100_000, 8_000_000, "Maestro"))
+    println(sizeCommission(100_000, typeCard = "Visa"))
+    println(sizeCommission(100_000, 0, "Мир"))
+    println(sizeCommission(100_000))
+
+}
+
+private fun sizeCommission(
+    amount: Int,
+    sumAmountMonth: Int = 0,
+    typeCard: String = "VkPay"
+): Number {
+    val minCommissionVisa = 3_500
+    val sizeCommissionVisa = 0.0075
+    val sizeCommissionMastercard = 0.006
+    val necessaryCommissionMastercard = 2_000
+
+    return when (typeCard) {
+        "Mastercard", "Maestro" -> if (sumAmountMonth < 7_500_000) 0 else amount * sizeCommissionMastercard + necessaryCommissionMastercard
+        "Visa", "Мир" -> if (amount * sizeCommissionVisa > minCommissionVisa) amount * sizeCommissionVisa else minCommissionVisa
+        else -> 0
+    }
+
+}
